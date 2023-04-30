50f64bb96ad92a2ecb1f9ce493a910bf1fdd2754
AhmadWaleed96 <ahmad.wal96@gmail.com>
2023-04-30 15:29:55 +0300

    first ahmad

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -5,24 +5,6 @@
       <!-- start section hero -->
 <section class="hero">
 
-    <div class="side-icons">
-        <div class="whatsapp icon">
-            <i class="fa-brands fa-whatsapp"></i>
-            <span class="content">
-                <a href="https://wa.link/igzsii" target="_blank">(+966) 56 138 8363</a>
-            </span>
-        </div>
-        <div class="phone icon">
-            <i class="fa fa-phone"></i>
-            <span class="content">
-                <a href="tel: (+966) 56 138 8363">(+966) 56 138 8363</a>
-            </span>
-        </div>
-        <div class="tarmeez icon">
-                                    <a href="https://tarmeeztech.com/tarmeez-tech-profile-ar.pdf" target="_blank"><img src="https://tarmeeztech.com/siteassets/images/icon.svg"></a>
-                            </div>
-    </div>
-
     <div class="Tarmmeez-Tach-p">
         <p class="we-are">We are <span>Tarmmeez Tach</span></p>
         <p class="paragraph-hero">Some people spend a lot of time searching for software companies specialized in creating and developing custom websites, electronic stores, designing mobile applications, or any service they wish to implement. </p>
@@ -32,10 +14,10 @@
             <a class="btn btn-contact" href="">Contact Us</a>
         </div>
     </div>
-    <img class="back-hero" src="image/back-hero.png" alt="">
-    <img class="img-hero" src="image/hero1.png" alt="">
-    <img class="line-hero" src="image/line-hero.png" alt="">
-    <img class="star-hero" src="image/star-hero.png" alt="">
+    <img class="back-hero" src="{{ asset('front/image/back-hero.png') }}" alt="">
+    <img class="img-hero" src="{{ asset('front/image/hero1.png') }}" alt="">
+    <img class="line-hero" src="{{ asset('front/image/line-hero.png') }}" alt="">
+    <img class="star-hero" src="{{ asset('front/image/star-hero.png') }}" alt="">
 </section>
 <!-- end section hero -->
 
@@ -47,7 +29,7 @@
         <div class="paragraph">
             <p>The key to being a brand that not only makes a good impression, but also refuses to evaporate from the ever-volatile industry, is partnering with a  </p>
         </div>
-        <img class="col-12 col-sm-12 col-md-12 col-lg-12" src="image/Design-Build-Run 1.png" alt="">
+        <img class="col-12 col-sm-12 col-md-12 col-lg-12" src="{{ asset('front/image/Design-Build-Run 1.png') }}" alt="">
 
         <a class="btn btn-more" href="">See more</a>
     </div>
@@ -184,110 +166,69 @@
 
 <!-- start section Featured -->
 <section class="Featured">
-    <div class="title">
-        <h1>Our Featured Work</h1>
+    <div class="featured-link">
+        <div class="title">
+            <h1>Our Featured Work</h1>
 
-        <div class="nav-Featured d-flex justify-content-center">
-                <li class="featured-item">
-                  <a class="featured-link active" aria-current="page" href="#">Website</a>
-                </li>
-                <li class="featured-item">
-                  <a class="featured-link" href="#">Application</a>
-                </li>
-                <li class="featured-item">
-                  <a class="featured-link" href="">Graphic design</a>
-                </li>
+            <div class="nav-Featured d-flex justify-content-center">
+                    <li class="featured-item">
+                      <a class="featured-link active" aria-current="page" href="#">Website</a>
+                    </li>
+                    <li class="featured-item">
+                      <a class="featured-link" href="#">Application</a>
+                    </li>
+                    <li class="featured-item">
+                      <a class="featured-link" href="">Graphic design</a>
+                    </li>
+            </div>
         </div>
     </div>
-    <img class="Featured-back" src="{{asset("front/>image/Group 56.png")}}" alt="">
-    <div class="featured-background">
-        <div>
-        <div class="swiper-button-next"></div>
-        <div class="swiper-button-prev"></div>
-        </div>
-        <div><a class="featured-btn" href="">See More</a></div>
-    </div>
-
-        <div class="Featured-card d-flex justify-content-center position-relative">
+    <div class="Featured-cont">
         <div class="container">
-            <div class="swiper myswiper">
-                <div class="swiper-wrapper">
-                  <div class="big swiper-slide">
+            <div class="services-slider">
+                <div>
                     <div class="card" style="width: 18rem;">
-                        <img src="{{asset("front/image/Imgae Place Holder.png")}}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <img src="{{ asset('front/image/Imgae Place Holder.png') }}" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </div>
+                            </div>
                         </div>
-                      </div>
-                  </div>
-                  <div class="swiper-slide">
+                <div>
                     <div class="card" style="width: 18rem;">
-                        <img src="{{asset("front/image/Imgae Place Holder (1).png")}}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <img src="{{ asset('front/image/Imgae Place Holder.png') }}" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </div>
+                            </div>
                         </div>
-                      </div>
-                  </div>
-                  <div class="swiper-slide">
+                <div>
                     <div class="card" style="width: 18rem;">
-                        <img src="{{asset("front/image/Imgae Place Holder (2).png")}}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <img src="{{ asset('front/image/Imgae Place Holder.png') }}" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </div>
+                            </div>
                         </div>
-                      </div>
-                  </div>
-                  <div class="swiper-slide">
+                <div>
                     <div class="card" style="width: 18rem;">
-                        <img src="{{asset("front/image/Imgae Place Holder.png")}}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <img src="{{ asset('front/image/Imgae Place Holder.png') }}" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </div>
+                            </div>
                         </div>
-                      </div>
-                  </div>
-                  <div class="swiper-slide">
+                <div>
                     <div class="card" style="width: 18rem;">
-                        <img src="{{asset("front/image/Imgae Place Holder (1).png")}}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <img src="{{ asset('front/image/Imgae Place Holder.png') }}" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </div>
+                            </div>
                         </div>
-                      </div>
-                  </div>
-                  <div class="swiper-slide">
-                    <div class="card" style="width: 18rem;">
-                        <img src="{{asset("front/image/Imgae Place Holder (2).png")}}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                      </div>
-                  </div>
-                  <div class="swiper-slide">
-                    <div class="card" style="width: 18rem;">
-                        <img src="{{asset("front/image/Imgae Place Holder.png")}}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                      </div>
-                  </div>
-                  <div class="swiper-slide">
-                    <div class="card" style="width: 18rem;">
-                        <img src="{{asset("front/image/Imgae Place Holder (1).png")}}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                      </div>
-                  </div>
-                  <div class="swiper-slide">
-                    <div class="card" style="width: 18rem;">
-                        <img src="{{asset("front/image/Imgae Place Holder (2).png")}}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                      </div>
-                  </div>
-                </div>
-
-              </div>
+            </div>
         </div>
+        <a class="featured-btn" href="">See More</a>
     </div>
 </section>
 <!-- end section Featured -->
@@ -323,92 +264,59 @@
 <!-- end section consultation -->
 
 <!-- start section success partners -->
-<section class="success-partners d-flex align-items-center">
-    <div>
-        <p class="success p-3" style="margin: 0 20px; font-size: 22px; font-weight: 700; color: #1D3867; width: 220px;">Success Partners</p>
+<section class="success-partners d-flex align-items-center overflow-hidden">
+    <div class="cont-success" style="width: 220px;">
+        <p class="success p-3" style="margin: 0 20px; font-size: 22px; font-weight: 700; color: #1D3867;">Success Partners</p>
     </div>
-    <div class="swiper myswiper">
-        <!-- Additional required wrapper -->
-        <div class="swiper-wrapper">
-          <!-- Slides -->
-          <div class="swiper-slide">
-            <div class="img-success">
-                <img src="{{asset('front/image/image 4.png')}}" alt="">
-            </div>
-          </div>
-          <div class="swiper-slide">
-            <div class="img-success">
-                <img src="{{asset('front/image/image 3.png')}}" alt="">
-            </div>
-          </div>
-          <div class="swiper-slide">
+    <div class="partners-slide col-10">
+        <div>
             <div class="img-success">
                 <img src="{{asset('front/image/image 1.png')}}" alt="">
             </div>
-          </div>
-          <div class="swiper-slide">
-            <div class="img-success">
-                <img src="{{asset("front/image/image 4.png")}}" alt="">
-            </div>
-          </div>
-          <div class="swiper-slide">
-            <div class="img-success">
-                <img src="{{asset("front/image/image 3.png")}}" alt="">
-            </div>
-          </div>
-          <div class="swiper-slide">
-            <div class="img-success">
-                <img src="{{asset("front/image/image 1.png")}}" alt="">
-            </div>
-          </div>
-          <div class="swiper-slide">
-            <div class="img-success">
-                <img src="{{asset('front/image/image 4.png')}}" alt="">
-            </div>
-          </div>
-          <div class="swiper-slide">
-            <div class="img-success">
-                <img src="{{asset('front/image/image 3.png')}}" alt="">
-            </div>
-          </div>
-          <div class="swiper-slide">
-            <div class="img-success">
-                <img src="{{asset('front/image/image 1.png')}}" alt="">
-            </div>
-          </div>
-          <div class="swiper-slide">
-            <div class="img-success">
-                <img src="{{asset('front/image/image 4.png')}}" alt="">
-            </div>
-          </div>
-          <div class="swiper-slide">
-            <div class="img-success">
-                <img src="{{asset('front/image/image 3.png')}}" alt="">
-            </div>
-          </div>
-          <div class="swiper-slide">
-            <div class="img-success">
-                <img src="{{asset('front/image/image 1.png')}}" alt="">
-            </div>
-          </div>
-          <div class="swiper-slide">
-            <div class="img-success">
-                <img src="{{asset('front/image/image 4.png')}}" alt="">
-            </div>
-          </div>
-          <div class="swiper-slide">
-            <div class="img-success">
-                <img src="{{asset('front/image/image 3.png')}}" alt="">
-            </div>
-          </div>
-          <div class="swiper-slide">
-            <div class="img-success">
-                <img src="{{asset('front/image/image 1.png')}}" alt="">
-            </div>
-          </div>
         </div>
-      </div>
+        <div>
+            <div class="img-success">
+                <img src="{{asset('front/image/image 3.png')}}" alt="">
+            </div>
+        </div>
+        <div>
+            <div class="img-success">
+                <img src="{{asset('front/image/image 4.png')}}" alt="">
+            </div>
+        </div>
+        <div>
+            <div class="img-success">
+                <img src="{{asset('front/image/image 1.png')}}" alt="">
+            </div>
+        </div>
+        <div>
+            <div class="img-success">
+                <img src="{{asset('front/image/image 3.png')}}" alt="">
+            </div>
+        </div>
+        <div>
+            <div class="img-success">
+                <img src="{{asset('front/image/image 4.png')}}" alt="">
+            </div>
+        </div>
+        <div>
+            <div class="img-success">
+                <img src="{{asset('front/image/image 1.png')}}" alt="">
+            </div>
+        </div>
+        <div>
+            <div class="img-success">
+                <img src="{{asset('front/image/image 3.png')}}" alt="">
+            </div>
+        </div>
+        <div>
+            <div class="img-success">
+                <img src="{{asset('front/image/image 4.png')}}" alt="">
+            </div>
+        </div>
+    </div>
 </section>
+
 <!-- end section success partners -->
 
 @endsection
--- resources/views/front/master.blade.php
+++ resources/views/front/master.blade.php
@@ -4,11 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="image/trmeez-icon.png" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('front/image/trmeez-icon.png') }}" type="image/x-icon">
     <link href="{{ asset('front/css/bootstrap.min.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"/>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"/>
     <link rel="stylesheet" href="{{ asset('front/css/style.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('slick/slick.css') }}"/>
+    <link rel="stylesheet" type="text/css" href="{{ asset('slick/slick-theme.css') }}"/>
     <title>Tarmmeez Tach</title>
 </head>
 
@@ -18,10 +20,10 @@
         <div class="container-fluid">
             <div class="nav-item dropdown dropdown1">
                 <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  العربية
+                  English
                 </a>
                 <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">English</a></li>
+                  <li><a class="dropdown-item" href="#">العربية</a></li>
                 </ul>
               </div>
           <a class="navbar-brand" href="#">
@@ -39,7 +41,51 @@
                 <a class="nav-link" href="{{ route('index-about') }}">About us</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link " href="{{ route('index') }}">Services</a>
+                <div class="nav-item dropdown-services">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Services
+                    </a>
+                    <div class="dropdown-menu menu-services">
+                        <div class="container cont-services">
+                              <span>
+                                <div class="img-down">
+                                    <img src="{{ asset('front/image/Online Shop.png') }}" alt="">
+                                </div>
+                                <a class="dropdown-link" href="#">Online store design</a>
+                              </span>
+                              <span>
+                                <div class="img-down">
+                                    <img src="{{ asset('front/image/Web Design.png') }}" alt="">
+                                </div>
+                                <a class="dropdown-link" href="#">Web Design</a>
+                              </span>
+                              <span>
+                                <div class="img-down">
+                                    <img src="{{ asset('front/image/Responsive Website.png') }}" alt="">
+                                </div>
+                                <a class="dropdown-link" href="#">Mobile application design</a>
+                              </span>
+                              <span>
+                                <div class="img-down">
+                                    <img src="{{ asset('front/image/Business Card.png') }}" alt="">
+                                </div>
+                                <a class="dropdown-link" href="#">Business identity</a>
+                              </span>
+                              <span>
+                                <div class="img-down">
+                                    <img src="{{ asset('front/image/Seo.png') }}" alt="">
+                                </div>
+                                <a class="dropdown-link" href="#">search engine optimization</a>
+                              </span>
+                              <span>
+                                <div class="img-down">
+                                    <img src="{{ asset('front/image/Voice.png') }}" alt="">
+                                </div>
+                                <a class="dropdown-link" href="#">Content writing and voiceover</a>
+                              </span>
+                        </div>
+                    </div>
+                  </div>
               </li>
               <li class="nav-item">
                 <a class="nav-link " href="{{ route('index-work') }}">Work</a>
@@ -63,10 +109,10 @@
                 </div>
                 <div class="nav-item dropdown d-none d-sm-none d-md-none d-lg-none d-xl-flex">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                      العربية
+                      English
                     </a>
                     <ul class="dropdown-menu">
-                      <li><a class="dropdown-item" href="#">English</a></li>
+                      <li><a class="dropdown-item" href="#">العربية</a></li>
                     </ul>
                   </div>
             </form>
@@ -86,7 +132,7 @@
     <div class="content d-flex flex-wrap" style="padding: 50px;">
         <div class="srart col-12 col-sm-12 col-md-6 col-lg-6">
             <a href="">
-                <img src="{{ asset("frontimage/logo.png") }}" alt="">
+                <img src="{{ asset("front/image/logo.png") }}" alt="">
             </a>
             <div class="cnt" style="padding: 40px 0;">
                 <div class="d-flex" style="padding: 15px 0;">
@@ -134,6 +180,23 @@
             </div>
         </div>
     </div>
+    <div class="side-icons">
+        <div class="whatsapp icon">
+            <i class="fa-brands fa-whatsapp"></i>
+            <span class="content">
+                <a href="https://wa.link/igzsii" target="_blank">(+966) 56 138 8363</a>
+            </span>
+        </div>
+        <div class="phone icon">
+            <i class="fa fa-phone"></i>
+            <span class="content">
+                <a href="tel: (+966) 56 138 8363">(+966) 56 138 8363</a>
+            </span>
+        </div>
+        <div class="tarmeez icon">
+            <a href="https://tarmeeztech.com/tarmeez-tech-profile-ar.pdf" target="_blank"><img src="https://tarmeeztech.com/siteassets/images/icon.svg"></a>
+        </div>
+    </div>
     <div class="bottom-footer w-100 bg-white text-center">
         <p style="color: #003366; font-size: 20px; margin: 0; padding: 17px;">Copy@2023-2022</p>
     </div>
@@ -146,37 +209,91 @@
   <i class="fas fa-arrow-up"></i>
 </button>
 </footer>
-<script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
 
-<script>
-    const swiper = new Swiper('.myswiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
+<script type="text/javascript" src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
+  <script type="text/javascript" src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+  <script type="text/javascript" src="{{ asset('slick/slick.min.js') }}"></script>
 
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
+<script type="text/javascript">
+  $(document).ready(function(){
+    $('.services-slider').slick({
+      arrows: false,
+      autoplay: true,
+      dots: true,
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
     },
-
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
     },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+      }
+    }
+  ]
+    });
 
+    $('.partners-slide').slick({
+      arrows: false,
+      autoplay: true,
+      dots: true,
+      slidesToShow: 6,
+      responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    }
+  ]
+    });
+  });
 
-        slidesPerView: 3,
-        spaceBetween: 30,
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-        autoplay: {
-          delay: 2500,
-          disableOnInteraction: false,
-        },
-      });
 </script>
     <script src="{{ asset('front/js/bootstrap.bundle.min.js') }}"></script>
     <script src="{{ asset('front/js/app.js') }}"></script>
